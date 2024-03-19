49d01d6bafe831f8b1ef2df36157b7d31da3e3f6
MuneebAhmed84942 <144244139+MuneebAhmed84942@users.noreply.github.com>
2024-03-19 06:42:23 +0400

    Update urls.py

--- exchange_application/urls.py
+++ exchange_application/urls.py
@@ -20,4 +20,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
     path('student/', include('student.urls')),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
