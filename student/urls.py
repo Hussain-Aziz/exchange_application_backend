91f4acb366b048ba9d0e8cc918f30686018aa172
Hussain Aziz <hussain.azizsaif@gmail.com>
2024-03-19 07:15:54 +0400

    fix url paths and requirements

--- student/urls.py
+++ student/urls.py
@@ -6,9 +6,7 @@ from django.urls import path
 from .views import StartApplicationAPI
 
 urlpatterns = [
-    path('http://localhost:3000/student/start_application/', StartApplicationAPI.as_view(), name='start-application'),
-    path('http://localhost:3000/student/add_courses/', StartApplicationAPI.as_view(), name='add_courses'),
-     path('http://localhost:3000/login/register/', StartApplicationAPI.as_view(), name='register'),
-
+    path('start_application/', StartApplicationAPI.as_view(), name='start-application'),
+    path('add_courses/', StartApplicationAPI.as_view(), name='add_courses'),
 ]
 
