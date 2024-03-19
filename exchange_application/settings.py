be63a8bf117e2656ee2e368a8b4c3b7ed1632fa7
MuneebAhmed84942 <144244139+MuneebAhmed84942@users.noreply.github.com>
2024-03-19 06:43:13 +0400

    Update settings.py

--- exchange_application/settings.py
+++ exchange_application/settings.py
@@ -46,6 +46,15 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ]
+}
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
