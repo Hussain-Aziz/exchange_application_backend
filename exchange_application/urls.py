3c561d2141e439f521c277cac4d6c83e54fcf0a7
Ayah12 <ayah_saif@hotmail.com>
2024-04-20 20:07:13 +0400

    commit

--- exchange_application/urls.py
+++ exchange_application/urls.py
@@ -17,7 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 from knox import views as knox_views
 from .views import LoginView
-
+from django.conf import settings # new
+from  django.conf.urls.static import static #new
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/', LoginView.as_view(), name='knox_login'),
@@ -27,3 +28,6 @@ urlpatterns = [
     path('student/', include('student.urls')),
     path('faculty/', include('faculty.urls')),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_URL)
