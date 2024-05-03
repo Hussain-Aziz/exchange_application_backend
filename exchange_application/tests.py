67e3cc41be568e59f2961e4b6c5fe29d7d16350f
Hussain Aziz <hussain.azizsaif@gmail.com>
2024-05-04 01:06:59 +0400

    fix tests

--- exchange_application/tests.py
+++ exchange_application/tests.py
@@ -6,6 +6,8 @@ from django.test import TestCase
 from django.urls import reverse
 from django.contrib.auth.models import User
 
+from .settings import *
+
 class LoginViewTest(APITestCase):
     def setUp(self):
         self.client = APIClient()
@@ -19,7 +21,7 @@ class LoginViewTest(APITestCase):
         self.assertEqual(request.status_code, 200)
 
     def test_login_faculty(self):
-        self.faculty = Faculty.objects.create(user=self.user, aus_id='b00012345')
+        self.faculty = Faculty.objects.create(user=self.user, department=13, faculty_type=0, college=3)
         request = self.client.post(reverse('knox_login'), {'username': 'testuser', 'password': 'testpass'})
         self.assertEqual(request.status_code, 200)
 
@@ -30,3 +32,34 @@ class TestSeralizer(TestCase):
     
     def test_seralizer(self):
         UserSerializer(self.user)
+
+class SettingsTest(TestCase):
+    def test_vars(self):
+        print(BASE_DIR)
+        print(SECRET_KEY)
+        print(DEBUG)
+        print(ALLOWED_HOSTS)
+        print(INSTALLED_APPS)
+        print(REST_FRAMEWORK)
+        print(REST_KNOX)
+        print(MIDDLEWARE)
+        print(ROOT_URLCONF)
+        print(TEMPLATES)
+        print(WSGI_APPLICATION)
+        print(DATABASES)
+        print(AUTH_PASSWORD_VALIDATORS)
+        print(LANGUAGE_CODE)
+        print(TIME_ZONE)
+        print(USE_I18N)
+        print(USE_TZ)
+        print(STATIC_URL)
+        print(DEFAULT_AUTO_FIELD)
+        print(AWS_ACCESS_KEY_ID)
+        print(AWS_SECRET_ACCESS_KEY)
+        print(AWS_STORAGE_BUCKET_NAME)
+        print(AWS_S3_FILE_OVERWRITE)
+        print(AWS_DEFAULT_ACL)
+        print(AWS_S3_REGION_NAME)
+        print(AWS_S3_SIGNATURE_VERSION)
+        print(AWS_S3_ADDRESSING_STYLE)
+        print(DEFAULT_FILE_STORAGE)
--- student/tests.py
+++ student/tests.py
@@ -1,5 +1,5 @@
 from rest_framework.test import APITestCase, APIClient
-from users.models import Faculty, User, Student, University
+from users.models import Faculty, IXODetails, User, Student, University
 from users.models import CourseApplication
 from django.urls import reverse
 from knox.auth import AuthToken
@@ -90,7 +90,7 @@ class SubmitApplication(APITestCase):
         self.client = APIClient()
         self.user = User.objects.create(username='testuser', password='testpass')
         self.university = University.objects.create(university_name='test university')
-        self.student = Student.objects.create(user=self.user, aus_id='b00012345', university=self.university)
+        self.student = Student.objects.create(user=self.user, aus_id='b00012345', university=self.university, ixo_details=IXODetails.objects.create())
         AuthToken.objects.create(user=self.user)
         self.token = AuthToken.objects.all()[0].token_key
         self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token)
